4127e7bc7eaaf3f000e101b44ee0f5db2725264b
Steven Noonan <stevennoonan013@gmail.com>
2017-10-26 16:05:03 +0100

    Started tests

--- tests.rkt
+++ tests.rkt
@@ -14,13 +14,45 @@
   (begin
     (display "Running tests...\n")
     ;begin calling test functions
-    (printf "1: ~a" (test_ins_beg1))
+    (printf "1: ~a~n" (test_ins_beg1))
+    (printf "1: ~a~n" (test_ins_beg2))
+    (printf "2: ~a~n" (test_ins_end1))
+    (printf "2: ~a~n" (test_ins_end2))
+    (printf "3: ~a~n" (test_cout_top_level1))
+    (printf "3: ~a~n" (test_cout_top_level2))
+    (printf "4: ~a~n" (test_count_instances1))
+    (printf "4: ~a~n" (test_count_instances2))
+    (printf "5: ~a~n" (test_count_instances_tr1))
+    (printf "5: ~a~n" (test_count_instances_tr2))
     ;end calling test functions
     (display "\nTests complete!\n")))
 
 ;Begin test functions
-(define (test_ins_beg1)
-  (eq? (ins_beg 1 '(2 3 4)) '(1 2 3 4)))
+;true test
+(define (test_ins_beg1)(equal? '(a b c d) (ins_beg 'a '(b c d))))
+;false test
+(define (test_ins_beg2)(equal? '(b c d) (ins_beg 'a '(b c d))))
+
+;true test
+(define (test_ins_end1) (equal? '(a b c d ) (ins_end 'd '(a b c))))
+;false test
+(define (test_ins_end2) (equal? '(d b c a ) (ins_end 'd '(a b c))))
+
+;true test
+(define (test_cout_top_level1) (equal? 4 (cout_top_level '(a b c d))))
+;false test
+(define (test_cout_top_level2) (equal? 3 (cout_top_level '(a b c d))))
+
+;true test
+(define (test_count_instances1) (equal? 5 (count_instances 'a '(a a a b b c c d a a))))
+;false test
+(define (test_count_instances2) (equal? 15 (count_instances 'a '(a a a b b c c d a a))))
+
+;true test
+(define (test_count_instances_tr1) (equal? 5 (count_instances_tr 'a '(a a a b b c c d a a))))
+;false test
+(define (test_count_instances_tr2) (equal? 15 (count_instances_tr 'a '(a a a b b c c d a a))))
+
 
 ;End test functions
 
